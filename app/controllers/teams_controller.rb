20aa2ae5f87145a02cd4c8e2e2793e0e2a944536
ogurayusuke <oguyu2021@gmail.com>
2023-07-20 18:13:22 +0900

    issue2.3実装完了

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,15 +1,20 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_team, only: %i[show edit update destroy]
+  before_action :set_team, only: %i[show edit update destroy assign_owner ]
 
   def index
     @teams = Team.all
   end
 
   def assign_owner
-    @team.update(owner_id: params[:owner_id])
-    @user = User.find(@team.owner_id)
-    redirect_to team_path, notice: 'オーナー権限が移動しました!'
+    new_owner = User.find(params[:owner_id])
+    if current_user == @team.owner && new_owner != @team.owner
+      @team.update(owner: new_owner)
+      TeamOwnerNotificationMailer.notify_new_owner(@team, new_owner).deliver_now
+      redirect_to team_path(@team.id), notice: '権限を移動しました！'
+    else
+      redirect_to team_path(@team.id), alert: '権限移動に失敗しました。'
+    end
   end
 
   def show
@@ -21,9 +26,9 @@ class TeamsController < ApplicationController
     @team = Team.new
   end
 
-  def edit; 
+  def edit
     unless @team.owner == current_user
-    redirect_to @team, notice: I18n.t('views.messages.leader_only_edit')
+    redirect_to team_path(@team.id), notice: I18n.t('views.messages.leader_only_edit')
     end
   end
 
@@ -32,7 +37,7 @@ class TeamsController < ApplicationController
     @team.owner = current_user
     if @team.save
       @team.invite_member(@team.owner)
-      redirect_to @team, notice: I18n.t('views.messages.create_team')
+      redirect_to team_path(@team.id), notice: I18n.t('views.messages.create_team')
     else
       flash.now[:error] = I18n.t('views.messages.failed_to_save_team')
       render :new
@@ -41,7 +46,7 @@ class TeamsController < ApplicationController
 
   def update
     if @team.update(team_params)
-      redirect_to @team, notice: I18n.t('views.messages.update_team')
+      redirect_to team_path(@team.id), notice: I18n.t('views.messages.update_team')
     else
       flash.now[:error] = I18n.t('views.messages.failed_to_save_team')
       render :edit
@@ -49,8 +54,12 @@ class TeamsController < ApplicationController
   end
 
   def destroy
-    @team.destroy
-    redirect_to teams_url, notice: I18n.t('views.messages.delete_team')
+    if current_user == @team.owner || current_user == @team.assigns.find_by(user_id: params[:user_id]).user
+      @team.destroy
+      redirect_to teams_url, notice: 'Team was successfully destroyed.'
+    else
+      redirect_to team_path(@team.id), alert: 'You do not have permission to delete this team.'
+    end
   end
 
   def dashboard
